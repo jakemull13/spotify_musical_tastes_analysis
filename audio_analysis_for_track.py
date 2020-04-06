7e34614e1e95b138726fd98f2868e464c88728e7
Jacob Mullins <jacobmullins@Jacobs-MacBook-Air.local>
2020-04-06 12:03:03 -0400

    analysis function

--- audio_analysis_for_track.py
+++ audio_analysis_for_track.py
@@ -1,21 +1,11 @@
 # shows audio analysis for the given track
-
 from __future__ import print_function    # (at top of module)
-from spotipy.oauth2 import SpotifyClientCredentials
 import json
-import spotipy
 import time
 import sys
 
 
-client_credentials_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-if len(sys.argv) > 1:
-    tid = sys.argv[1]
-else:
-    tid = 'spotify:track:4TTV7EcfroSLWzXRY6gLv6'
-
+tid = id
 start = time.time()
 analysis = sp.audio_analysis(tid)
 delta = time.time() - start
